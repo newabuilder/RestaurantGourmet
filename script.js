5d2ab104714532aa76de14b149c5740481eef012
newabuilder <neywalkin07@gmail.com>
2024-09-15 22:09:21 -0500

    Codigo comentado

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
+// Contenedor de formularios
 const logregBox = document.querySelector('.logreg-box');
 
+// Enlaces para cambiar entre formularios
 const loginLink = document.querySelector('.login-link');
-
 const registerLink = document.querySelector('.register-link');
 
+// Muestra el formulario de registro
 registerLink.addEventListener('click', () => {
     logregBox.classList.add('active');
 });
 
+// Muestra el formulario de inicio de sesión
 loginLink.addEventListener('click', () => {
     logregBox.classList.remove('active');
 });
